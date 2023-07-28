44546aab1b76929a758bb2a6598518943452ace6
Vladimir Kalnitsky <vladimir@mlabs.city>
2023-07-28 16:22:43 +0400

    Use CommonJS modules

--- browser/index.js
+++ browser/index.js
@@ -1,37 +1,4 @@
-const process = (lib) => {
-  const finRegistry = new FinalizationRegistry((x) => {
-    try {
-      x.free();
-    } catch (_) {}
-  });
+const csl = require("@emurgo/cardano-serialization-lib-browser");
+const wrappedCsl = require('@mlabs-haskell/csl-gc-wrapper')(csl);
 
-  const classWrap = (classObj) => {
-    Object.getOwnPropertyNames(classObj).forEach((propName) => {
-      if (propName === "__wrap") {
-        const oldMethod = classObj[propName];
-        classObj[propName] = function () {
-          const retVal = oldMethod.apply(classObj, arguments);
-          if (retVal && retVal.ptr) {
-            const px = new Proxy(retVal, {});
-            finRegistry.register(px, retVal, px);
-            return px;
-          }
-          return retVal;
-        };
-      }
-    });
-    return classObj;
-  };
-
-  Object.keys(lib).forEach((k) => {
-    if (k[0].toUpperCase() == k[0] && k[0] != "_") {
-      classWrap(lib[k]);
-    }
-  });
-
-  return lib;
-};
-
-const lib = process(await import("@emurgo/cardano-serialization-lib-browser"));
-
-export default lib;
+module.exports = wrappedCsl;
