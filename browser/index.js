5530f2c657e36637775ea0245ce19ea796427da1
Dzmitry Shuysky <errfrom@yandex.ru>
2024-06-05 11:28:16 +0200

    Bump CSL to v12.0.0-alpha.31, Fix exports gen code

--- browser/index.js
+++ browser/index.js
@@ -2,20 +2,6 @@ const csl = await import("@emurgo/cardano-serialization-lib-browser");
 const f = await import('@mlabs-haskell/csl-gc-wrapper');
 const lib = f.default(csl);
 
-// Explicit re-exports are needed due to how nodejs processes CommonJS imports in ESM.
-// If they are dynamic, they are only available via the `default` property of the module.
-// Providing them explicitly lets nodejs analyze the module structure and put the
-// appropriate entry points to the module interface.
-
-// To re-generate the code below use this snippet:
-
-// for (let key in lib) {
-//     if (lib.hasOwnProperty(key)) {
-//         module.exports[key] = lib[key];
-//         if (!key.startsWith('_'))
-//         console.log('export const ' + key + ' = lib["'+key+'"];');
-//     }
-// }
 
 export const Address = lib["Address"];
 export const AddressKind = lib["AddressKind"];
